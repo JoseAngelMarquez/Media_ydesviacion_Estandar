d5e4828a50e8acacfc70750d54665f49edcada0f
Jośe Ángel <joseangelmarquezespina060503@gmail.com>
2023-03-07 16:43:26 -0600

    Coigo corregido(correcciones)

--- src/dev/Media.java
+++ src/dev/Media.java
@@ -1,41 +1,62 @@
 package dev;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Media{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Media {
+	
 
-        // Pedir al usuario el número de elementos
-        System.out.print("Ingrese la cantidad de elementos: ");
-        int n = sc.nextInt();
 
-        // Crear un arreglo para almacenar los elementos
-        double[] numeros = new double[n];
-
-        // Pedir al usuario que ingrese los números
-        for (int i = 0; i < n; i++) {
-            System.out.print("Ingrese el número " + (i + 1) + ": ");
-            numeros[i] = sc.nextDouble();
-        }
-
-        // Calcular la media
-        double suma = 0;
-        for (int i = 0; i < n; i++) {
-            suma += numeros[i];
-        }
-        double media = suma / n;
-
-        // Calcular la suma de los cuadrados de las diferencias
-        double sumaCuadradosDiferencias = 0;
-        for (int i = 0; i < n; i++) {
-            sumaCuadradosDiferencias += Math.pow(numeros[i] - media, 2);
-        }
-
-        // Calcular la desviación estándar
-        double desviacionEstandar = Math.sqrt(sumaCuadradosDiferencias / n);
-
-        // Mostrar el resultado
-        System.out.println("La desviación estándar es: " + desviacionEstandar);
-    }
-}
+	   public static void main(String[] args) {
+	       
+	       Scanner input = new Scanner(System.in);
+	       
+	       System.out.print("Ingresa la cantidad de números: ");
+	       int n = input.nextInt();
+	       
+	       double suma = 0.0;
+	       double[] numeros = new double[n];
+	       
+	       for(int i = 0; i < n; i++){
+	           System.out.print("Ingresa un número: ");
+	           numeros[i] = input.nextDouble();
+	           suma += numeros[i];
+	       }
+	       
+	       double media = suma / n;
+	       double sumaCuadrados = 0.0;
+	       
+	       for(int i = 0; i < n; i++){
+	           sumaCuadrados += Math.pow(numeros[i] - media, 2);
+	       }
+	       
+	       double desviacion = Math.sqrt(sumaCuadrados / (n - 1));
+	       
+	       Arrays.sort(numeros);
+	       int maxVeces = 1;
+	       int veces = 1;
+	       double moda = numeros[0];
+	       
+	       for(int i = 1; i < n; i++) {
+	           if(numeros[i] == numeros[i-1]) {
+	               veces++;
+	               if(veces > maxVeces) {
+	                   maxVeces = veces;
+	                   moda = numeros[i];
+	               }
+	           } else {
+	               veces = 1;
+	           }
+	       }
+	       
+	       if(maxVeces == 1) {
+	           System.out.println("No hay moda");
+	       } else {
+	           System.out.println("La moda es: " + moda);
+	       }
+	       
+	       System.out.println("La media aritmética es: " + media);
+	       System.out.println("La desviación estándar es: " + desviacion);
+	       
+	       input.close();
+	   }
+	}
